84f98e5a12b8609aafd12e4fdea70ca6a363139f
Andreas Sander <mail@andi1984.de>
2021-01-30 18:20:23 +0100

    Svelte - Add active state to the nav items

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,39 +1,22 @@
 <script lang="ts">
+  import NavItem from "./NavItem.svelte";
   export let segment: string;
 </script>
+
+<nav>
+  <ul>
+    <NavItem active={segment === undefined} label="Privat" href="." />
+    <NavItem active={segment === "webdev"} label="Webdev" href="webdev" />
+    <NavItem
+      active={segment === "streaming"}
+      label="Streaming"
+      href="streaming"
+    />
+  </ul>
+</nav>
 
 <style>
   nav {
     margin-bottom: 5px;
   }
 </style>
-
-<nav>
-  <ul>
-    <li>
-      <a aria-current={segment === undefined ? "page" : undefined} href="."
-        >Privat</a
-      >
-    </li>
-    <li>
-      <a aria-current={segment === "webdev" ? "page" : undefined} href="webdev"
-        >Webdev</a
-      >
-    </li>
-    <li>
-      <a
-        aria-current={segment === "streaming" ? "page" : undefined}
-        href="streaming">Streaming</a
-      >
-    </li>
-
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-    <!-- <li>
-      <a
-        rel="prefetch"
-        aria-current={segment === 'blog' ? 'page' : undefined}
-        href="blog">blog</a>
-    </li> -->
-  </ul>
-</nav>
---
+++ src/components/NavItem.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let active: boolean;
+  export let label: string;
+  export let href: string;
+</script>
+
+<li class={active ? "active" : undefined}>
+  <a aria-current={active ? "page" : undefined} {href}>{label}</a>
+</li>
